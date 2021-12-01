b3981c08978de7c4f7e016b16779eb07437b2f9c
Ramya <ramyamalengri@gmail.com>
2021-12-02 00:11:45 +0500

    I did this

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hai");
+        }
+    }
+}
